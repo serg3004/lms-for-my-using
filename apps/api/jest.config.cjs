cc53b3867ec147a250016cef00235416e96058e8
serg3004 <afanaskovserg@gmail.com>
2026-05-25 13:25:02 +0500

    test(api): map users test imports

--- apps/api/jest.config.cjs
+++ apps/api/jest.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
-    '^\\.\\/organizations\\.schemas\\.js$': '<rootDir>/src/modules/organizations/organizations.schemas.ts',
-    '^\\.\\/organizations\\.service\\.js$': '<rootDir>/src/modules/organizations/organizations.service.ts',
-    '^\\.\\.\\/\\..\\/database/prisma\\.service\\.js$': '<rootDir>/src/database/prisma.service.ts',
+    '^\\./organizations\\.schemas\\.js$': '<rootDir>/src/modules/organizations/organizations.schemas.ts',
+    '^\\./organizations\\.service\\.js$': '<rootDir>/src/modules/organizations/organizations.service.ts',
+    '^\\./users\\.schemas\\.js$': '<rootDir>/src/modules/users/users.schemas.ts',
+    '^\\./users\\.service\\.js$': '<rootDir>/src/modules/users/users.service.ts',
+    '^\\.\\./\\.\\./database/prisma\\.service\\.js$': '<rootDir>/src/database/prisma.service.ts',
   },
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
