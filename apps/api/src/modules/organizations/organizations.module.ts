e8c76a9cd8775889277ab2022fab73dcd321a3c2
serg3004 <afanaskovserg@gmail.com>
2026-05-25 17:11:54 +0500

    feat(api): protect organization read endpoints

--- apps/api/src/modules/organizations/organizations.module.ts
+++ apps/api/src/modules/organizations/organizations.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestqs/common';
 
+import { AuthModule } from '../auth/auth.module.js';
 import { OrganizationsController } from './organizations.controller.js';
 import { OrganizationsService } from './organizations.service.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
 })
