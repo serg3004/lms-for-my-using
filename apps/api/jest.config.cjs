fb86bab7c65c7010296c9f3f47e56d456eaff279
serg3004 <afanaskovserg@gmail.com>
2026-05-25 10:46:23 +0500

    test(api): configure jest for api tests

---
+++ apps/api/jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extensionsToTreatAsEsm: ['.ts'],
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+        useESM: true,
+      },
+    ],
+  },
+};
