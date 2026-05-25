a3db96a876b381f54c737ae4035a185e99cf3dd3
serg3004 <afanaskovserg@gmail.com>
2026-05-25 14:04:29 +0500

    feat(api): add auth foundation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './database/database.module.js';
+import { AuthModule } from './modules/auth/auth.module.js';
 import { HealthModule } from './modules/health/health.module.js';
 import { MembershipsModule } from './modules/memberships/memberships.module.js';
 import { OrganizationsModule } from './modules/organizations/organizations.module.js';
 import { UsersModule } from './modules/users/users.module.js';
 
 @Module({
-  imports: [DatabaseModule, HealthModule, MembershipsModule, OrganizationsModule, UsersModule],
+  imports: [DatabaseModule, AuthModule, HealthModule, MembershipsModule, OrganizationsModule, UsersModule],
 })
 export class AppModule {}
