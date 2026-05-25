dbe049c2362aa481590cfad9b26683f596d6d7b1
serg3004 <afanaskovserg@gmail.com>
2026-05-25 17:11:18 +0500

    feat(api): export jwt auth guard

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller.js';
+import { AuthGuard } from './auth.guard.js';
 import { AuthService } from './auth.service.js';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthGuard, AuthService],
+  exports: [AuthGuard, AuthService],
 })
 export class AuthModule {}
