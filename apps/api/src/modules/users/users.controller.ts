4a88a81a90118c622d37f903685d5f528e4a9eda
serg3004 <afanaskovserg@gmail.com>
2026-05-25 20:04:20 +0500

    feat(api): apply rbac policies to current endpoints

--- apps/api/src/modules/users/users.controller.ts
+++ apps/api/src/modules/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
 import { AuthGuard } from '../auth/auth.guard.js';
+import { Roles, rolePolicies } from '../auth/roles.js';
+import { RolesGuard } from '../auth/roles.guard.js';
 import { createUserSchema, CreateUserInput } from './users.schemas.js';
 import { UsersService } from './users.service.js';
 
@@ -9,13 +11,15 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(...rolePolicies.usersRead)
   listUsers() {
     return this.usersService.listUsers();
   }
 
   @Get(':id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(...rolePolicies.usersRead)
   getUser(@Param('id') userId: string) {
     return this.usersService.getUser(userId);
   }
