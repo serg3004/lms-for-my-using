0b96de1be4a2ae273930a0e68f0d39d46e18e2d1
serg3004 <afanaskovserg@gmail.com>
2026-05-25 13:45:03 +0500

    feat(api): add memberships api module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './database/database.module.js';
 import { HealthModule } from './modules/health/health.module.js';
+import { MembershipsModule } from './modules/memberships/memberships.module.js';
 import { OrganizationsModule } from './modules/organizations/organizations.module.js';
 import { UsersModule } from './modules/users/users.module.js';
 
 @Module({
-  imports: [DatabaseModule, HealthModule, OrganizationsModule, UsersModule],
+  imports: [DatabaseModule, HealthModule, MembershipsModule, OrganizationsModule, UsersModule],
 })
 export class AppModule {}
