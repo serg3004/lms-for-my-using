374a548b6f3347027f6edb88fc686095db259797
serg3004 <afanaskovserg@gmail.com>
2026-05-25 20:04:45 +0500

    feat(api): apply rbac policies to current endpoints

--- apps/api/src/modules/memberships/memberships.controller.ts
+++ apps/api/src/modules/memberships/memberships.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
 import { AuthGuard } from '../auth/auth.guard.js';
+import { Roles, rolePolicies } from '../auth/roles.js';
+import { RolesGuard } from '../auth/roles.guard.js';
 import {
   CreateMembershipInput,
   createMembershipSchema,
@@ -8,21 +10,24 @@ import {
 import { MembershipsService } from './memberships.service.js';
 
 @Controller('memberships')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
 export class MembershipsController {
   constructor(private readonly membershipsService: MembershipsService) {}
 
   @Get()
+  @Roles(...rolePolicies.membershipsRead)
   listMemberships() {
     return this.membershipsService.listMemberships();
   }
 
   @Get(':id')
+  @Roles(...rolePolicies.membershipsRead)
   getMembership(@Param('id') membershipId: string) {
     return this.membershipsService.getMembership(membershipId);
   }
 
   @Post()
+  @Roles(...rolePolicies.membershipsCreate)
   createMembership(@Body() body: unknown) {
     const input: CreateMembershipInput = createMembershipSchema.parse(body);
 
