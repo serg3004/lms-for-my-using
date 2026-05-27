8bc5879a9f9c86bb25f9af74fe183310579c018d
serg3004 <afanaskovserg@gmail.com>
2026-05-27 14:11:26 +0500

    feat(api): register OpenAPI module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -13,6 +13,7 @@ import { GroupsModule } from './modules/groups/groups.module.js';
 import { HealthModule } from './modules/health/health.module.js';
 import { LessonsModule } from './modules/lessons/lessons.module.js';
 import { MembershipsModule } from './modules/memberships/memberships.module.js';
+import { OpenApiModule } from './modules/openapi/openapi.module.js';
 import { OrganizationsModule } from './modules/organizations/organizations.module.js';
 import { ProgressModule } from './modules/progress/progress.module.js';
 import { UsersModule } from './modules/users/users.module.js';
@@ -32,6 +33,7 @@ import { UsersModule } from './modules/users/users.module.js';
     HealthModule,
     LessonsModule,
     MembershipsModule,
+    OpenApiModule,
     OrganizationsModule,
     ProgressModule,
     UsersModule,
