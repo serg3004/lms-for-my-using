e694b621b84a809279d2e70d095733252ebb0462
serg3004 <afanaskovserg@gmail.com>
2026-05-27 13:43:33 +0500

    feat(api): register centralized API exception filter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module.js';
+import { ApiExceptionFilter } from './common/filters/api-exception.filter.js';
 import { loadApiEnv } from './config/env.js';
 
 async function bootstrap(): Promise<void> {
@@ -9,6 +10,7 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new ApiExceptionFilter());
 
   await app.listen(apiEnv.API_PORT);
 }
