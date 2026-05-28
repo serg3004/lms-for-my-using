74b4d7ba2d515df51f74b38f8ca9847aadc15f99
serg3004 <afanaskovserg@gmail.com>
2026-05-28 13:40:28 +0500

    feat(web): route learner home

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,21 +1,28 @@
 import { useTranslation } from 'react-i18next';
 
+import { LearnerHomePage } from './LearnerHomePage.js';
 import { LoginPage } from './LoginPage.js';
 
 export function App() {
   const { t } = useTranslation();
+  const pathname = window.location.pathname;
 
-  if (window.location.pathname === '/login') {
+  if (pathname === '/login') {
     return <LoginPage />;
+  }
+
+  if (pathname === '/learn') {
+    return <LearnerHomePage />;
   }
 
   return (
     <main>
       <h1>{t('app.title')}</h1>
       <p>{t('app.subtitle')}</p>
-      <p>
+      <nav>
         <a href="/login">{t('login.navLink')}</a>
-      </p>
+        <a href="/learn">{t('learner.navLink')}</a>
+      </nav>
     </main>
   );
 }
