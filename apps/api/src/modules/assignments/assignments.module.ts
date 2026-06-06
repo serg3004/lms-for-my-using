f67c6324535a2d0c936dcf787d588ab09c25b130
serg3004 <afanaskovserg@gmail.com>
2026-06-06 13:38:01 +0500

    fix(api): import auth module in assignments module

--- apps/api/src/modules/assignments/assignments.module.ts
+++ apps/api/src/modules/assignments/assignments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 import { AssignmentsController } from './assignments.controller.js';
 import { AssignmentsService } from './assignments.service.js';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AssignmentsController],
   providers: [AssignmentsService],
 })
