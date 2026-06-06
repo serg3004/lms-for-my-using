6c8825e5ffbb2428d8750f3efcc75c37547f929c
serg3004 <afanaskovserg@gmail.com>
2026-06-06 11:32:10 +0500

    fix(api): import auth module in upload module

--- apps/api/src/modules/upload/upload.module.ts
+++ apps/api/src/modules/upload/upload.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module.js';
 import { UploadController } from './upload.controller.js';
 import { UploadService } from './upload.service.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
