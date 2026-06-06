d892ad674eddf2a93841345da164f30cfcabec4f
serg3004 <afanaskovserg@gmail.com>
2026-06-06 13:37:44 +0500

    fix(api): import auth module in remaining guarded modules

--- apps/api/src/modules/assessment-questions/assessment-questions.module.ts
+++ apps/api/src/modules/assessment-questions/assessment-questions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 import { AssessmentQuestionsController } from './assessment-questions.controller.js';
 import { AssessmentQuestionsService } from './assessment-questions.service.js';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AssessmentQuestionsController],
   providers: [AssessmentQuestionsService],
 })
